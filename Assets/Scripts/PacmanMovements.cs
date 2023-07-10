3bb4b226dabe5e783981ec54a1ca51c5f4712cb6
Mateus Oliveira <devmateusalves@gmail.com>
2023-07-10 01:29:36 -0300

    pacman movements

---
+++ Assets/Scripts/PacmanMovements.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMovements : MonoBehaviour {
+
+    private float moveX, moveY;
+    private float moveSpeed = 1f;
+
+    private SpriteRenderer sprite;
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.gravityScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+
+        // Invert flip position
+        this.Flip();
+    }
+
+
+    // Invert sprites
+    private void Flip() {
+        float angle;
+        if (moveX != 0) {
+            angle = moveX > 0 ? 0f : 180f;
+            SetRotation(angle);
+        } else if (moveY != 0) {
+            angle = moveY > 0 ? 90f : 270;
+            SetRotation(angle);
+        } 
+    }
+
+
+    private void SetRotation(float angle) {
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+
+    void FixedUpdate(){
+        if (moveX != 0) {
+            rigidBody.velocity = new Vector2(moveX * moveSpeed, 0);
+        } else if (moveY != 0) {
+            rigidBody.velocity = new Vector2(0, moveY * moveSpeed);
+        } 
+    }
+}
